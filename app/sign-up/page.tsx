6ff307c1aa39453ff8c4979e5a1627656dcea57a
Abshir <abshir.ahmed@somalisintech.com>
2023-08-15 02:04:54 +0100

    add sign up page
    CAAWI-3

---
+++ app/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button';
+
+export default function SignUp() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button>sign up</Button>
+    </main>
+  );
+}

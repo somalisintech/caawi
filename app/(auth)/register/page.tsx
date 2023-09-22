a286b445c0cc10a6d7322d5e6635d1446ec4f12c
Abshir <abshir.ahmed@somalisintech.com>
2023-09-22 19:56:09 +0100

    use tabs in registrations page

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -14,6 +14,7 @@ import { useEffect } from 'react';
 import axios from 'axios';
 import { redirect } from 'next/navigation';
 import Link from 'next/link';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 export default function Register() {
   const { status } = useSession();
@@ -83,144 +84,246 @@ export default function Register() {
         <div className="mx-auto w-full max-w-sm lg:w-96">
           <h2 className="mt-6 text-3xl font-bold tracking-tight text-gray-900">Register for an account</h2>
           <div className="mt-8">
-            <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
-                <div className="flex w-full max-w-sm items-center space-x-2">
-                  <FormField
-                    control={form.control}
-                    name="firstName"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel className="block text-sm font-medium text-gray-700">First Name</FormLabel>
-                        <FormControl>
-                          <Input {...field} type={'text'} autoFocus />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="lastName"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel className="block text-sm font-medium text-gray-700">Last Name</FormLabel>
-                        <FormControl>
-                          <Input {...field} type={'text'} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
+            <Tabs defaultValue="mentee" className="w-[400px]">
+              <TabsList className="grid w-full grid-cols-2">
+                <TabsTrigger value="mentee">Mentee</TabsTrigger>
+                <TabsTrigger value="mentor">Mentor</TabsTrigger>
+              </TabsList>
+              <TabsContent value="mentee">
+                <p className="my-5 font-normal text-gray-600">
+                  Dive into your growth journey! Sign up as a <span className="font-semibold">mentee</span> and discover
+                  the ideal mentor for your path ahead.
+                </p>
+                <Form {...form}>
+                  <form id="mentee-registration-form" onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+                    <div className="flex w-full max-w-sm items-center space-x-2">
+                      <FormField
+                        control={form.control}
+                        name="firstName"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel className="block text-sm font-medium text-gray-700">First Name</FormLabel>
+                            <FormControl>
+                              <Input {...field} type={'text'} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="lastName"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel className="block text-sm font-medium text-gray-700">Last Name</FormLabel>
+                            <FormControl>
+                              <Input {...field} type={'text'} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel className="block text-sm font-medium text-gray-700">Email address</FormLabel>
+                          <FormControl>
+                            <Input {...field} type={'email'} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel className="block text-sm font-medium text-gray-700">Password</FormLabel>
+                          <FormControl>
+                            <Input {...field} type={'password'} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="confirm"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel className="block text-sm font-medium text-gray-700">Confirm Password</FormLabel>
+                          <FormControl>
+                            <Input {...field} type={'password'} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <div>
+                      <Button className="mt-3 w-full" type="submit">
+                        {form.formState.isSubmitting ? (
+                          <FaSpinner className="animate-spin" size={'1.5em'} />
+                        ) : (
+                          'Register'
+                        )}
+                      </Button>
+                    </div>
+                  </form>
+                </Form>
+                <div className="relative mt-6">
+                  <div className="absolute inset-0 flex items-center" aria-hidden="true">
+                    <div className="w-full border-t border-gray-300" />
+                  </div>
+                  <div className="relative flex justify-center text-sm">
+                    <span className="bg-white px-2 text-gray-500">Or continue with</span>
+                  </div>
                 </div>
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel className="block text-sm font-medium text-gray-700">Email address</FormLabel>
-                      <FormControl>
-                        <Input {...field} type={'email'} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel className="block text-sm font-medium text-gray-700">Password</FormLabel>
-                      <FormControl>
-                        <Input {...field} type={'password'} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="confirm"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel className="block text-sm font-medium text-gray-700">Confirm Password</FormLabel>
-                      <FormControl>
-                        <Input {...field} type={'password'} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <div>
-                  <Button className="mt-3 w-full" type="submit">
-                    {form.formState.isSubmitting ? <FaSpinner className="animate-spin" size={'1.5em'} /> : 'Sign up'}
+                <div className="mt-6 grid grid-cols-4 gap-3">
+                  <Button
+                    className="text-gray-500 hover:text-gray-500"
+                    variant="outline"
+                    title="Sign in with Google"
+                    onClick={() =>
+                      signIn('google', {
+                        redirect: false,
+                        callbackUrl: '/profile'
+                      })
+                    }
+                  >
+                    <FaGoogle size={'1.5em'} />
+                  </Button>
+                  <Button
+                    className="text-gray-500 hover:text-gray-500"
+                    variant="outline"
+                    title="Sign in with Twitter"
+                    onClick={() =>
+                      signIn('twitter', {
+                        redirect: false,
+                        callbackUrl: '/profile'
+                      })
+                    }
+                  >
+                    <FaTwitter size={'1.5em'} />
+                  </Button>
+                  <Button
+                    className="text-gray-500 hover:text-gray-500"
+                    variant="outline"
+                    title="Sign in with GitHub"
+                    onClick={() =>
+                      signIn('github', {
+                        redirect: false,
+                        callbackUrl: '/profile'
+                      })
+                    }
+                  >
+                    <FaGithub size={'1.5em'} />
+                  </Button>
+                  <Button
+                    className="text-gray-500 hover:text-gray-500"
+                    variant="outline"
+                    title="Sign in with LinkedIn"
+                    onClick={() =>
+                      signIn('linkedin', {
+                        redirect: false,
+                        callbackUrl: '/profile'
+                      })
+                    }
+                  >
+                    <FaLinkedin size={'1.5em'} />
                   </Button>
                 </div>
-              </form>
-            </Form>
-            <div className="relative mt-6">
-              <div className="absolute inset-0 flex items-center" aria-hidden="true">
-                <div className="w-full border-t border-gray-300" />
-              </div>
-              <div className="relative flex justify-center text-sm">
-                <span className="bg-white px-2 text-gray-500">Or continue with</span>
-              </div>
-            </div>
-            <div className="mt-6 grid grid-cols-4 gap-3">
-              <Button
-                className="text-gray-500 hover:text-gray-500"
-                variant="outline"
-                title="Sign in with Google"
-                onClick={() =>
-                  signIn('google', {
-                    redirect: false,
-                    callbackUrl: '/profile'
-                  })
-                }
-              >
-                <FaGoogle size={'1.5em'} />
-              </Button>
-              <Button
-                className="text-gray-500 hover:text-gray-500"
-                variant="outline"
-                title="Sign in with Twitter"
-                onClick={() =>
-                  signIn('twitter', {
-                    redirect: false,
-                    callbackUrl: '/profile'
-                  })
-                }
-              >
-                <FaTwitter size={'1.5em'} />
-              </Button>
-              <Button
-                className="text-gray-500 hover:text-gray-500"
-                variant="outline"
-                title="Sign in with GitHub"
-                onClick={() =>
-                  signIn('github', {
-                    redirect: false,
-                    callbackUrl: '/profile'
-                  })
-                }
-              >
-                <FaGithub size={'1.5em'} />
-              </Button>
-              <Button
-                className="text-gray-500 hover:text-gray-500"
-                variant="outline"
-                title="Sign in with LinkedIn"
-                onClick={() =>
-                  signIn('linkedin', {
-                    redirect: false,
-                    callbackUrl: '/profile'
-                  })
-                }
-              >
-                <FaLinkedin size={'1.5em'} />
-              </Button>
-            </div>
+              </TabsContent>
+              <TabsContent value="mentor">
+                <p className="my-5 font-normal text-gray-600">
+                  Share your experience! Register as a <span className="font-semibold">mentor</span> and support
+                  individuals keen on advancing their journey.
+                </p>
+                <Form {...form}>
+                  <form id="mentor-registration-form" onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+                    <div className="flex w-full max-w-sm items-center space-x-2">
+                      <FormField
+                        control={form.control}
+                        name="firstName"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel className="block text-sm font-medium text-gray-700">First Name</FormLabel>
+                            <FormControl>
+                              <Input {...field} type={'text'} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="lastName"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel className="block text-sm font-medium text-gray-700">Last Name</FormLabel>
+                            <FormControl>
+                              <Input {...field} type={'text'} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel className="block text-sm font-medium text-gray-700">Email address</FormLabel>
+                          <FormControl>
+                            <Input {...field} type={'email'} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel className="block text-sm font-medium text-gray-700">Password</FormLabel>
+                          <FormControl>
+                            <Input {...field} type={'password'} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="confirm"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel className="block text-sm font-medium text-gray-700">Confirm Password</FormLabel>
+                          <FormControl>
+                            <Input {...field} type={'password'} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <div>
+                      <Button className="mt-3 w-full" type="submit">
+                        {form.formState.isSubmitting ? (
+                          <FaSpinner className="animate-spin" size={'1.5em'} />
+                        ) : (
+                          'Register'
+                        )}
+                      </Button>
+                    </div>
+                  </form>
+                </Form>
+              </TabsContent>
+            </Tabs>
           </div>
           <p className="mt-6 text-center text-sm leading-6 text-gray-500">
             Already have an account?{' '}

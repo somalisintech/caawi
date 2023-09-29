1e772ea3544ef6fc992559df4b5eb41be1f71871
Abshir <abshir.ahmed@somalisintech.com>
2023-09-29 15:23:14 +0100

    add signin-bg-image.jpg

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -172,7 +172,7 @@ export default function Login() {
       <div className="relative hidden w-0 flex-1 lg:block">
         <Image
           className="absolute inset-0 h-full w-full object-cover"
-          src="https://images.unsplash.com/photo-1556711905-b3f402e1ff80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2970&q=80"
+          src="/signin-bg-image.jpg"
           alt="Photo by Prateek Katyal on Unsplash"
           fill={true}
           priority

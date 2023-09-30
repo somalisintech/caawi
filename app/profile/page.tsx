dfe1389b28acb577c7a1b5f0d0f27ef506c442ea
Abshir <abshir.ahmed@somalisintech.com>
2023-09-30 03:36:13 +0100

    update profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,15 +1,46 @@
 import { Separator } from '@/components/ui/separator';
 import { ProfileForm } from './profile-form';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+import { Badge } from '@/components/ui/badge';
+import prisma from '@/lib/db';
 
-export default function SettingsProfilePage() {
+export default async function SettingsProfilePage() {
+  const session = await getServerSession(authOptions);
+  if (!session) return redirect('/login');
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: session.user.id
+    },
+    select: {
+      name: true,
+      firstName: true,
+      lastName: true,
+      email: true,
+      image: true,
+      createdAt: true,
+      updatedAt: true,
+      profile: true
+    }
+  });
+
+  if (!user) return redirect('/login');
+
   return (
     <div className="space-y-6">
       <div>
-        <h3 className="text-lg font-medium">Profile</h3>
+        <div className="flex">
+          <h3 className="text-lg font-medium">Profile</h3>
+          <Badge variant="outline" className="ml-auto">
+            {user.profile.userType}
+          </Badge>
+        </div>
         <p className="text-sm text-muted-foreground">This is how others will see you on the site.</p>
       </div>
       <Separator />
-      <ProfileForm />
+      <ProfileForm {...user} />
     </div>
   );
 }
--- app/profile/profile-form.tsx
+++ app/profile/profile-form.tsx
@@ -1,24 +1,20 @@
 'use client';
 
-import Link from 'next/link';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useFieldArray, useForm } from 'react-hook-form';
 import * as z from 'zod';
-
 import { cn } from '@/lib/utils';
 import { toast } from '@/components/ui/use-toast';
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
+import { RxTrash } from 'react-icons/rx';
+import { Profile } from '@prisma/client';
 
 const profileFormSchema = z.object({
-  email: z
-    .string({
-      required_error: 'Please select an email to display.'
-    })
-    .email(),
+  firstName: z.string().min(1, { message: 'First Name is required' }),
+  lastName: z.string().min(1, { message: 'Last Name is required' }),
   bio: z.string().max(160).min(4),
   urls: z
     .array(
@@ -33,17 +29,34 @@ type ProfileFormValues = z.infer<typeof profileFormSchema>;
 
 // This can come from your database or API.
 const defaultValues: Partial<ProfileFormValues> = {
-  bio: 'I own a computer.',
-  urls: [{ value: 'https://shadcn.com' }, { value: 'http://twitter.com/shadcn' }]
+  firstName: '',
+  lastName: '',
+  bio: '',
+  urls: [{ value: '' }]
 };
 
-export function ProfileForm() {
+interface Props {
+  name: string | null;
+  firstName: string | null;
+  lastName: string | null;
+  email: string | null;
+  image: string | null;
+  createdAt: Date | null;
+  updatedAt: Date | null;
+  profile: Profile | null;
+}
+
+export function ProfileForm({ firstName, lastName, profile }: Props) {
+  defaultValues.firstName = firstName || '';
+  defaultValues.lastName = lastName || '';
+  defaultValues.bio = profile?.bio || '';
+
   const form = useForm<ProfileFormValues>({
     resolver: zodResolver(profileFormSchema),
     defaultValues
   });
 
-  const { fields, append } = useFieldArray({
+  const { fields, append, remove } = useFieldArray({
     name: 'urls',
     control: form.control
   });
@@ -63,32 +76,34 @@ export function ProfileForm() {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <Select onValueChange={field.onChange} defaultValue={field.value}>
+        <div className="flex w-full max-w-sm items-center space-x-2">
+          <FormField
+            control={form.control}
+            name="firstName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="block text-sm font-medium text-gray-700">First Name</FormLabel>
                 <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select a verified email to display" />
-                  </SelectTrigger>
+                  <Input {...field} type={'text'} />
                 </FormControl>
-                <SelectContent>
-                  <SelectItem value="m@example.com">m@example.com</SelectItem>
-                  <SelectItem value="m@google.com">m@google.com</SelectItem>
-                  <SelectItem value="m@support.com">m@support.com</SelectItem>
-                </SelectContent>
-              </Select>
-              <FormDescription>
-                You can manage verified email addresses in your{' '}
-                <Link href="/profile/notifications">email settings</Link>.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="lastName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="block text-sm font-medium text-gray-700">Last Name</FormLabel>
+                <FormControl>
+                  <Input {...field} type={'text'} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
         <FormField
           control={form.control}
           name="bio"
@@ -98,9 +113,6 @@ export function ProfileForm() {
               <FormControl>
                 <Textarea placeholder="Tell us a little bit about yourself" className="resize-none" {...field} />
               </FormControl>
-              <FormDescription>
-                You can <span>@mention</span> other users and organizations to link to them.
-              </FormDescription>
               <FormMessage />
             </FormItem>
           )}
@@ -118,7 +130,18 @@ export function ProfileForm() {
                     Add links to your website, blog, or social media profiles.
                   </FormDescription>
                   <FormControl>
-                    <Input {...field} />
+                    <div className="flex w-full max-w-sm items-center space-x-1">
+                      <Input {...field} />
+                      <Button
+                        variant="secondary"
+                        type="button"
+                        size="icon"
+                        onClick={() => remove(index)}
+                        title="Delete URL"
+                      >
+                        <RxTrash size={16} />
+                      </Button>
+                    </div>
                   </FormControl>
                   <FormMessage />
                 </FormItem>

95e2158cf583dc93e3bc88301d4f03992d3a22bd
Abshir <abshir.ahmed@somalisintech.com>
2024-02-25 16:36:53 +0000

    feat: add MentorProfile view and update schema (CAAWI-34)
    This commit introduces a new SQL view, MentorProfile, which selects
    mentor-specific details from various related tables. It also includes the
    addition of MentorProfile to the Prisma schema, enabling access to these details
    via Prisma client. A new migration file has been created to handle the
    creation of the MentorProfile view in the database.

---
+++ prisma/migrations/20240225154520_add_mentor_profile/migration.sql
@@ -0,0 +1,19 @@
+CREATE VIEW MentorProfile AS
+SELECT
+  p.id,
+  p."userType",
+  u."firstName",
+  u."lastName",
+  p.bio,
+  p.gender,
+  l.city,
+  l.country,
+  o.role ,
+  o."yearsOfExperience",
+  c.name AS company
+FROM "Profile" p
+       JOIN "User" u ON u."profileId" = p.id
+       LEFT JOIN "Location" l ON p."locationId" = l.id
+       LEFT JOIN "Occupation" o ON p."occupationId" = o.id
+       LEFT JOIN "Company" c ON o."companyId" = c.id
+WHERE p."userType" = 'MENTOR';

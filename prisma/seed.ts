2b237217a5ef9a4ece56a05fe6fafbdde9eef171
Abshir <abshir.ahmed@somalisintech.com>
2024-03-24 15:47:09 +0000

    feat(seed.ts): refactor seeding process for user profiles (CAAWI-57)

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
-import { $Enums, PrismaClient, UserType } from '@prisma/client';
+import { PrismaClient, UserType, Gender } from '@prisma/client';
 import { faker, Sex } from '@faker-js/faker';
-import Gender = $Enums.Gender;
+
 const prisma = new PrismaClient();
 
 async function main() {
@@ -10,43 +10,48 @@ async function main() {
 
   console.log('Start seeding 🌱');
 
-  const promises = Array.from({ length: 100 }, () => {
+  const promises = Array.from({ length: 100 }, async () => {
     const gender = faker.person.sex();
     const firstName = faker.person.firstName(gender as Sex);
     const lastName = faker.person.lastName(gender as Sex);
     const name = faker.person.fullName({ firstName, lastName });
-    const email = faker.internet.email({
-      firstName,
-      lastName
+    const email = faker.internet.email({ firstName, lastName });
+
+    const city = faker.location.city();
+    const country = faker.location.country();
+    const location = await prisma.location.upsert({
+      where: { country: country },
+      update: {},
+      create: { city: city, country: country }
     });
 
-    return prisma.user.create({
+    const role = faker.person.jobTitle();
+    const yearsOfExperience = faker.number.int({ min: 1, max: 30 });
+    const company = faker.company.name();
+    const occupation = await prisma.occupation.upsert({
+      where: { company: company },
+      update: {},
+      create: { role: role, yearsOfExperience: yearsOfExperience, company: company }
+    });
+
+    const profile = await prisma.profile.create({
+      data: {
+        userType: faker.helpers.enumValue(UserType),
+        bio: faker.person.bio(),
+        gender: gender.toUpperCase() as Gender,
+        locationId: location.id,
+        occupationId: occupation.id
+      }
+    });
+
+    await prisma.user.create({
       data: {
         name,
         firstName,
         lastName,
         email,
         image: faker.image.avatar(),
-        profile: {
-          create: {
-            userType: faker.helpers.enumValue(UserType),
-            bio: faker.person.bio(),
-            gender: gender.toUpperCase() as Gender,
-            location: {
-              create: {
-                city: faker.location.city(),
-                country: faker.location.country()
-              }
-            },
-            occupation: {
-              create: {
-                role: faker.person.jobTitle(),
-                yearsOfExperience: faker.number.int({ min: 1, max: 30 }),
-                company: faker.company.name()
-              }
-            }
-          }
-        }
+        profileId: profile.id
       }
     });
   });

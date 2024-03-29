68c5ea8b72d396ec04f05bfd8d0981700dd12648
nuurcodes <nuurcodes@gmail.com>
2024-03-29 17:19:21 +0400

    chore: move all envs to env config
    This commit moves all environments variables to the env config.
    
    CAAWI-33

--- config/env.ts
+++ config/env.ts
@@ -1,8 +1,11 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
+  NODE_ENV: z.string().catch('development'),
   CALENDLY_CLIENT_ID: z.string(),
-  CALENDLY_CLIENT_SECRET: z.string()
+  CALENDLY_CLIENT_SECRET: z.string(),
+  NEXT_PUBLIC_POSTHOG_KEY: z.string(),
+  NEXT_PUBLIC_POSTHOG_HOST: z.string()
 });
 
 export const env = envSchema.parse(process.env);
--- providers/posthog-provider.tsx
+++ providers/posthog-provider.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import posthog from 'posthog-js';
+import { env } from '@/config/env';
 import { PropsWithChildren } from 'react';
 import { PostHogProvider } from 'posthog-js/react';
 
+const { NEXT_PUBLIC_POSTHOG_HOST, NEXT_PUBLIC_POSTHOG_KEY } = env;
+
 if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST
+  posthog.init(NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: NEXT_PUBLIC_POSTHOG_HOST
   });
 }
 

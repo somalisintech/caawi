7ff95b79217a6458c8e3850e88e2c9de54ca0b49
nuurcodes <nuurcodes@gmail.com>
2024-03-30 21:34:52 +0400

    chore: remove env fallback
    This commit removes supabase env fallback.
    
    CAAWI-33

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -2,10 +2,7 @@ import { createBrowserClient } from '@supabase/ssr';
 
 // TODO: Figure out why client envs dont work after refresh
 export function createClient() {
-  const {
-    NEXT_PUBLIC_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
-  } = process.env;
+  const { NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY } = process.env;
 
-  return createBrowserClient(NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY);
+  return createBrowserClient(NEXT_PUBLIC_SUPABASE_URL!, NEXT_PUBLIC_SUPABASE_ANON_KEY!);
 }

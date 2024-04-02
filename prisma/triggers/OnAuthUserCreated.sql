1b72cd89b6b655b45d85f87d7114c8ec29415236
nuurcodes <nuurcodes@gmail.com>
2024-04-02 17:31:03 +0400

    feat: parse auth user fields
    This commit updates the auth trigger to parse auth user fields
    
    CAAWI-33

--- prisma/triggers/OnAuthUserCreated.sql
+++ prisma/triggers/OnAuthUserCreated.sql
@@ -6,13 +6,34 @@ SET search_path = public, auth
 AS $$
 DECLARE
   p_id uuid;
+  first_name text;
+  last_name text;
+  image_url text;
 BEGIN
   INSERT INTO public."Profile"
   DEFAULT VALUES
   RETURNING "id" into p_id;
 
+  IF NEW.raw_user_meta_data ? 'name' THEN
+    first_name := split_part(NEW.raw_user_meta_data ->> 'name', ' ', 1);
+    last_name := split_part(NEW.raw_user_meta_data ->> 'name', ' ', 2);
+  ELSE
+    first_name := NULL;
+    last_name := NULL;
+  END IF;
+
+  IF NEW.raw_user_meta_data ? 'avatar_url' THEN
+    image_url := NEW.raw_user_meta_data ->> 'avatar_url';
+  ELSE
+    IF NEW.raw_user_meta_data ? 'picture' THEN
+      image_url := NEW.raw_user_meta_data ->> 'picture';
+    ELSE
+      image_url := NULL;
+    END IF;
+  END IF;
+
   INSERT INTO public."User" ("id", "email", "firstName", "lastName", "image", "profileId")
-  VALUES (NEW.id, NEW.email, NEW.raw_user_meta_data ->> 'first_name', NEW.raw_user_meta_data ->> 'last_name', NEW.raw_user_meta_data ->> 'image', p_id);
+  VALUES (NEW.id, NEW.email, first_name, last_name, image_url, p_id);
   RETURN NEW;
 END;
 $$;

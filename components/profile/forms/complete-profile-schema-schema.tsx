18f5474c18fd5f5dd06c6a3ed219ac0470af29ca
NinjaNuur <71561866+nuurcodes@users.noreply.github.com>
2024-04-09 21:33:49 +0400

    feat: make bio required for mentors on signup (CAAWI-49) (#51)

--- components/profile/forms/complete-profile-schema-schema.tsx
+++ components/profile/forms/complete-profile-schema-schema.tsx
@@ -1,13 +1,23 @@
 import { z } from 'zod';
 import { Gender, UserType } from '@prisma/client';
 
-export const completeProfileFormSchema = z.object({
-  userType: z.nativeEnum(UserType, { invalid_type_error: 'Required' }),
-  firstName: z.string().min(1, { message: 'First name is required' }),
-  lastName: z.string().min(1, { message: 'Last name is required' }),
-  email: z.string().email({ message: 'Please enter a valid email address' }),
-  gender: z.nativeEnum(Gender, { invalid_type_error: 'Required' }),
-  bio: z.string().optional()
-});
+export const completeProfileFormSchema = z
+  .object({
+    userType: z.nativeEnum(UserType, { invalid_type_error: 'Required' }),
+    firstName: z.string().min(1, { message: 'First name is required' }),
+    lastName: z.string().min(1, { message: 'Last name is required' }),
+    email: z.string().email({ message: 'Please enter a valid email address' }),
+    gender: z.nativeEnum(Gender, { invalid_type_error: 'Required' }),
+    bio: z.string().optional()
+  })
+  .superRefine((data, ctx) => {
+    if (data.userType === UserType.MENTOR && !data.bio) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ['bio'],
+        message: 'Required'
+      });
+    }
+  });
 
 export type CompleteProfileFormFields = z.infer<typeof completeProfileFormSchema>;

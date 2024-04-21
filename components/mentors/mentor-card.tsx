a68c9db4b1962db41722f1e5d12e57bd186e3534
nuurcodes <nuurcodes@gmail.com>
2024-04-21 18:13:08 +0400

    fix: profile redirect (CAAWI-67)

--- components/mentors/mentor-card.tsx
+++ components/mentors/mentor-card.tsx
@@ -1,7 +1,7 @@
 import { MentorProfile } from '@prisma/client';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
-import { createClient } from '@/utils/supabase/client';
+import { createClient } from '@/utils/supabase/server';
 import { getUrl } from '@/utils/url';
 import Link from 'next/link';
 

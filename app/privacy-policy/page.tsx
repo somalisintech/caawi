243bc41ca305366dc03f1494c79c576cb5fbfb2b
Abshir <abshir.ahmed@somalisintech.com>
2024-05-26 11:33:35 +0100

    feat: add internal redirection to terms and privacy policy pages (CAAWI-103)

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default function PrivacyPolicyPage() {
+  return redirect('https://somalisintech.notion.site/Caawi-Privacy-Policy-ace5f3932a714b91b9c069dad66db3d4');
+}

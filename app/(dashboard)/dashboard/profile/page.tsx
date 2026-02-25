ae8a3797561831a401108f5501d1fab39dc67398
Mohamed Hassan <mohamedahassan1998@gmail.com>
2026-02-25 15:56:16 +0000

    fix: fix db query and types

--- app/(dashboard)/dashboard/profile/page.tsx
+++ app/(dashboard)/dashboard/profile/page.tsx
@@ -31,7 +31,7 @@ export default async function SettingsProfilePage() {
           location: true,
           occupation: true,
           calendlyUser: true,
-          skills: { select: { name: true } }
+          skills: true
         }
       }
     }
